b946aac0491d31556bb7a43bf381f4c57ad3729f
Samuel Rodríguez <71430352+DJStatikVX@users.noreply.github.com>
2024-12-24 19:13:58 +0100

    feat: add solution to challenge 16

---
+++ 16.js
@@ -0,0 +1,39 @@
+/*
+
+  Los elfos están trabajando arduamente para limpiar los caminos llenos de nieve mágica ❄️. Esta nieve tiene una propiedad especial: si dos montículos de nieve idénticos y adyacentes se encuentran, desaparecen automáticamente.
+  
+  Tu tarea es escribir una función que ayude a los elfos a simular este proceso. El camino se representa por una cadena de texto y cada montículo de nieve un carácter.
+  
+  Tienes que eliminar todos los montículos de nieve adyacentes que sean iguales hasta que no queden más movimientos posibles.
+  
+  El resultado debe ser el camino final después de haber eliminado todos los montículos duplicados:
+  
+  removeSnow('zxxzoz') // -> "oz"
+  // 1. Eliminamos "xx", quedando "zzoz"
+  // 2. Eliminamos "zz", quedando "oz"
+  
+  removeSnow('abcdd') // -> "abc"
+  // 1. Eliminamos "dd", quedando "abc"
+  
+  removeSnow('zzz') // -> "z"
+  // 1. Eliminamos "zz", quedando "z"
+  
+  removeSnow('a') // -> "a"
+  // No hay montículos repetidos
+
+*/
+
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+function removeSnow(s) {
+  let result = s
+  let snowIndex
+
+  while ((snowIndex = [...result].findIndex((char, index) => char === result[index + 1])) !== -1) {
+    result = result.substring(0, snowIndex) + result.substring(snowIndex + 2)
+  }
+
+  return result
+}
